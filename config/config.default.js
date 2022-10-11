2215a72897cec6152559aef93719bdd8c79a7553
willwan92 <wilson3cy@163.com>
2022-10-11 20:50:04 +0800

    修改代理配置

--- config/config.default.js
+++ config/config.default.js
@@ -34,6 +34,12 @@ module.exports = (appInfo) => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1625726121147_1965';
 
+  
+  // 注意，开启此模式后，应用就默认自己处于反向代理之后，
+  // 会支持通过解析约定的请求头来获取用户真实的 IP，协议和域名。
+  // 如果你的服务未部署在反向代理之后，请不要开启此配置，以防被恶意用户伪造请求 IP 等信息。
+  config.proxy = true;
+
   // add your middleware config here
   config.middleware = ['errorHandler'];
 
