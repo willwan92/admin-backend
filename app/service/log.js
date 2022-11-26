f7e7b55537187c863e8a4d4edb961a06cf256132
liujun <jun.liu@adsec.com.cn>
2022-11-26 11:40:53 +0800

    修改日志查询

--- app/service/log.js
+++ app/service/log.js
@@ -7,12 +7,12 @@ class LogService extends Service {
     const { ctx } = this;
     const where = {};
     const Op = ctx.app.Sequelize.Op;
-    query.date && (where.date = { [Op.substring]: `${query.date}` });
-    query.user && (where.user = { [Op.substring]: `${query.user}` });
-    query.sip && (where.sip = { [Op.substring]: `${query.sip}` });
-    query.msg && (where.msg = { [Op.substring]: `${query.msg}` });
-    query.pri && (where.pri = query.pri);
-    query.type && (where.type = query.type);
+    queryParams.date && (where.date = { [Op.substring]: `${queryParams.date}` });
+    queryParams.user && (where.user = { [Op.substring]: `${queryParams.user}` });
+    queryParams.sip && (where.sip = { [Op.substring]: `${queryParams.sip}` });
+    queryParams.msg && (where.msg = { [Op.substring]: `${queryParams.msg}` });
+    queryParams.pri && (where.pri = queryParams.pri);
+    queryParams.type && (where.type = queryParams.type);
     const attrs = ['date', 'user', 'sip', 'pri', 'type', 'msg'];
     const pageParams = {
       pageNo: query.pageNo,
