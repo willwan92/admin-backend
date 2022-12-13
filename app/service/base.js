775af3221a49af0b29f064bf0708d29ea14be173
willwan92 <wilson3cy@163.com>
2022-12-13 17:56:25 +0800

    修复异步执行后台命令异常处理bug

--- app/service/base.js
+++ app/service/base.js
@@ -49,50 +49,50 @@ class BaseService extends Service {
     }
   }
 
-   /**
+  /**
    * 分页查询数据
    * @param where 查询参数 Object
    * @param pageParams 分页参数 { pageNo, pageSize }
    * @param modelName 模型名称 String
    * @param attributes 可选。要查询的属性（字段）Array
-   * @param order 可选。排序方法 
+   * @param order 可选。排序方法
    * @param delegate 可选。数据库对应的delegate名称 String
    * @return
    */
-    async orderpage(
-      where,
-      pageParams,
-      modelName,
-      attributes = null,
-      order,
-      delegate = 'model'
-    ) {
-      try {
-        const limit = pageParams.pageSize ? Number(pageParams.pageSize) : 10;
-        const offset = pageParams.pageNo
-          ? (pageParams.pageNo - 1) * limit - 1
-          : 0;
-  
-        const { count, rows } = await this.ctx[delegate][
-          modelName
-        ].findAndCountAll({
-          where,
-          limit,
-          offset,
-          attributes,
-          order,
-        });
-        return {
-          total: count,
-          data: rows,
-          pageNo: pageParams.pageNo || 1,
-          pageSize: pageParams.pageSize || 10,
-        };
-      } catch (error) {
-        this.ctx.logger.error(error);
-        this.ctx.throw(500, '服务器错误');
-      }
+  async orderpage(
+    where,
+    pageParams,
+    modelName,
+    attributes = null,
+    order,
+    delegate = 'model'
+  ) {
+    try {
+      const limit = pageParams.pageSize ? Number(pageParams.pageSize) : 10;
+      const offset = pageParams.pageNo
+        ? (pageParams.pageNo - 1) * limit - 1
+        : 0;
+
+      const { count, rows } = await this.ctx[delegate][
+        modelName
+      ].findAndCountAll({
+        where,
+        limit,
+        offset,
+        attributes,
+        order,
+      });
+      return {
+        total: count,
+        data: rows,
+        pageNo: pageParams.pageNo || 1,
+        pageSize: pageParams.pageSize || 10,
+      };
+    } catch (error) {
+      this.ctx.logger.error(error);
+      this.ctx.throw(500, '服务器错误');
     }
+  }
 
   /**
    * 查询全部数据
@@ -101,16 +101,10 @@ class BaseService extends Service {
    * @param delegate 可选。数据库对应的delegate名称 String
    * @return
    */
-   async nopage(
-    modelName,
-    attributes = null,
-    delegate = 'model'
-  ) {
+  async nopage(modelName, attributes = null, delegate = 'model') {
     try {
-      const rows = await this.ctx[delegate][
-        modelName
-      ].findAll({
-        attributes
+      const rows = await this.ctx[delegate][modelName].findAll({
+        attributes,
       });
 
       return {
@@ -130,14 +124,18 @@ class BaseService extends Service {
    */
   execSync(cmd, args = []) {
     var cmdstr = '';
-    for (var i = 0; i < args.length; i++){
+    for (var i = 0; i < args.length; i++) {
       cmdstr += args[i] + ' ';
     }
-    fs.appendFile('/tmp/nodejs.cmd.txt',cmd + ' ' + cmdstr + '\n',function(err){
-      if(err){
-        return console.log('命令记录失败'+err.message);
+    fs.appendFile(
+      '/tmp/nodejs.cmd.txt',
+      cmd + ' ' + cmdstr + '\n',
+      function (err) {
+        if (err) {
+          return console.log('命令记录失败' + err.message);
+        }
       }
-    })
+    );
     const result = child_process.spawnSync(cmd, args, { encoding: 'utf-8' });
     const stdout = result.stdout ? result.stdout.toLocaleLowerCase() : '';
     if (result.error) {
@@ -162,31 +160,35 @@ class BaseService extends Service {
    */
   execAsync(cmd, args = []) {
     var cmdstr = '';
-    for (var i = 0; i < args.length; i++){
+    for (var i = 0; i < args.length; i++) {
       cmdstr += args[i] + ' ';
     }
-    fs.appendFile('/tmp/nodejs.cmd.txt',cmd + ' ' + cmdstr + '\n',function(err){
-      if(err){
-        return console.log('命令记录失败'+err.message);
+    fs.appendFile(
+      '/tmp/nodejs.cmd.txt',
+      cmd + ' ' + cmdstr + '\n',
+      function (err) {
+        if (err) {
+          return console.log('命令记录失败' + err.message);
+        }
       }
-    })
+    );
     return new Promise((resolve, reject) => {
       const result = child_process.spawn(cmd, args, { encoding: 'utf-8' });
 
       result.stdout.on('data', (data) => {
         let stdout = data.toString();
         stdout = stdout ? stdout.toLocaleLowerCase() : '';
-
-        if (stdout && (stdout.includes('错误') || stdout.includes('error'))) {
-          this.ctx.throw(455, result.stdout);
-        }
-
         // 去掉打印的换行符
         stdout = stdout.replace(/\n/g, '');
+
+        if (stdout && (stdout.includes('错误') || stdout.includes('error'))) {
+          reject(stdout);
+        }
+
         if (stdout === '0') {
-          resolve(data);
+          resolve(stdout);
         } else {
-          reject(data);
+          reject(stdout);
         }
       });
 
@@ -199,7 +201,6 @@ class BaseService extends Service {
       result.on('close', (code) => {
         // 执行失败并抛出错误
         this.ctx.logger.error(`执行失败，退出码：${code}`);
-        this.ctx.throw(500, '服务器错误');
       });
     });
   }
@@ -228,8 +229,8 @@ class BaseService extends Service {
     }
   }
   /**
-  *设备管理日志记录
-  */
+   *设备管理日志记录
+   */
   mnglog(level, message) {
     //const hostip = this.ctx.request.ip; //这里为什么提示ctx未定义
     const hostip = '1.1.1.1';
